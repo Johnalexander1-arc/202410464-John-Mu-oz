be2b9cbc09885c873af99c962e9e0642d2446799
Johnalexander1-arc <john.munoz04@uptc.edu.co>
2025-08-21 16:27:26 +0000

    ejercicio 2 hola mundo

---
+++ HelloWorld2/HelloWorld2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    cout <<"Hola mundo!: " <<endl;
+    cout <<"John Alexander Muñoz Rincon!: " <<endl;
+    cout <<"202410464" <<endl;
+    return 0;
+
+}
---
+++ HelloWorld2/helloWorld1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout<<"Hola mundo! " <<endl;
+    cout <<"soy un programa en c++" <<endl;
+    return 0;
+
+}
